7d23690c0a35c0031311056314ceeca2fd3b0af0
ZanabJafry <zs12gt@brocku.ca>
2016-12-19 05:56:31 -0500

    Lab 10

---
+++ Lab10Ex1.js
@@ -0,0 +1,13 @@
+
+
+function numberStuff() {
+
+
+var one = document.getElementById("firstnumber").value;
+var two = document.getElementById("secondnumber").value;
+var three = parseInt(one)+ parseInt(two);
+
+document.getElementById("output").innerHTML= three;
+
+
+}
