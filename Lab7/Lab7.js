50edc0a97b8000816a4ad67de1b90c689a24782b
Zanab Jafry <zanabism@gmail.com>
2016-12-24 05:52:53 -0500

    Lab 7 For week 7 Exercise 7

---
+++ Lab7/Lab7.js
@@ -0,0 +1,11 @@
+function textMagic() {
+
+
+document.getElementById("one").style.color = "blue";
+document.getElementById("one").style.fontSize = "100px";
+document.getElementById("two").style.color="green";
+document.getElementById("two").style.textAlign="center";
+document.getElementById("three").style.color="yellow";
+document.getElementById("three").style.textTransform="uppercase";
+
+}
